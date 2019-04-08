c1b4977d1b10a5b9cefa2a0062115876a51488a4
Braden Mars <BradenM@users.noreply.github.com>
2019-04-07 23:46:15 -0500

    style: Formatting

--- test/helpers/release-rules.js
+++ test/helpers/release-rules.js
@@ -1,4 +1,4 @@
-import proxyquire from 'proxyquire';
+import proxyquire from "proxyquire";
 
 /**
  * Return the `release-rules` array, replacing `types` with parameter.
@@ -8,5 +8,5 @@ import proxyquire from 'proxyquire';
  * @return {Array} the `release-rules` array for the `types` in parameter.
  */
 export default function releaseRules(types = {}) {
-	return proxyquire('../../release-rules', {'./types': {types}});
+  return proxyquire("../../release-rules", { "./types": { types } });
 }
